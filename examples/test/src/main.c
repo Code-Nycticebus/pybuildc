6041322c7f81753aab7f01bc97bed729bb5b7a1f
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-03-27 22:08:32 +0200

    removed examples folder

--- examples/test/src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "module/test.h"
-
-int main(void) {
-  printf("Hello\n");
-  printf("a = %d\n", get_a());
-  return 0;
-}
--- examples/test/src/module/test.c
+++
@@ -1,5 +0,0 @@
-#include "test.h"
-
-static int a = 1;
-
-int get_a(void) { return a; }
