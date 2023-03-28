79d157cdf7903a86d2739f0e4a1412b0e5adc0bb
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-03-28 07:06:05 +0200

    Corrected the install

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pybuildc",
+    version="0.1.0",
+    description="A Build system for the C language",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Loris Kriyonas",
+    author_email="loris.kriyonas@gmail.com",
+    keywords=["c"],
+    python_requires=">=3.10",
+    packages=find_packages(),
+    install_requires=[
+        "returns",
+        'importlib-metadata; python_version<"3.10"',
+    ],
+    entry_points={
+        "console_scripts": [
+            "pybuildc = pybuildc.main:main",
+        ]
+    },
+    setup_requires=[
+        "setuptools>=42",
+        "setuptools_scm>=3.5",
+    ],
+    use_scm_version={"write_to": "pybuildc/__version__.py"},
+)
