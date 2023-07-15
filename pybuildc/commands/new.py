7efa7e1e33dfc6e0b6172d0373a51cc7121ac467
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-07-15 23:46:39 +0200

    Now running 'build_compile_commands' at project init

--- pybuildc/commands/new.py
+++ pybuildc/commands/new.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from returns.io import IOResultE
 
+from pybuildc.domain.context import BuildContext
+from pybuildc.domain.builder import build_compile_commands
+
 
 def _create_file(file: Path, content: str) -> Path:
     file.parent.mkdir(parents=True, exist_ok=True)
@@ -14,16 +17,17 @@ def new(args) -> IOResultE[int]:
         Path(args.directory, "src", args.directory.with_suffix(".h").name),
         f"""\
 #pragma once
-#define LIBNAME "{args.directory.name}"
+#define {args.directory.name.capitalize()} "{args.directory.name}"
 """,
     )
+
     if not args.lib:
         _create_file(
             Path(args.directory, "src", "main.c"),
             f"""\
 #include "{args.directory.name}.h"
 #include <stdio.h>
-int main(void) {{ printf("Project: " LIBNAME "\\n"); }}
+int main(void) {{ printf("Project: " {args.directory.name.capitalize()} "\\n"); }}
 """,
         )
 
@@ -51,8 +55,12 @@ int main(void) {{ printf("Test: " LIBNAME "\\n"); }}
         Path(args.directory, ".clangd"),
         """\
 CompileFlags:
-    CompilationDatabase: ".build/"
+    CompilationDatabase: .build/
 """,
     )
 
-    return IOResultE.from_value(0)
+    return (
+        BuildContext.create_from_config(args.directory, "debug", False)
+        .map(build_compile_commands)
+        .map(lambda _: 0)
+    )
