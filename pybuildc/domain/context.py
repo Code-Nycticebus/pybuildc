218f52eebc1bb25005c97c6f063c6315e8454864
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-10-04 13:57:38 +0200

    Made a dependecie tree to only recompile the things i need

--- pybuildc/domain/context.py
+++ pybuildc/domain/context.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from pathlib import Path
 import pickle
+from collections import defaultdict
+from typing import Iterable
 
 from returns.io import IOResultE
 
@@ -24,6 +26,32 @@ def get_cache(directory: Path, target: str) -> dict[Path, float]:
     except FileNotFoundError:
         return dict()
 
+def build_dep_tree(files: Iterable[Path]) -> dict[Path, set]:
+    deps = defaultdict(set)
+    for file in files:
+        text = file.read_text()
+        find_text = "#include"
+        index = text.find(find_text)
+        index = text.find("\"", index)
+        while 0 <= index:
+            include_start = text[index+1:]
+            end_index = include_start.find("\"")
+            include = include_start[:end_index]
+            deps[file].add(Path(file.parent, include)) 
+
+            text = include_start
+            index = text.find(find_text)
+            index = text.find("\"", index)
+    return deps 
+
+def has_dep_that_changed(file: Path, deps: dict[Path, set], has_changed: set[Path]) -> bool:
+    if deps[file].intersection(has_changed):
+        return True
+    for dep in deps[file]:
+        if has_dep_that_changed(dep, deps, has_changed):
+            return True
+    return False
+
 
 def collect_cache(directory: Path, target: str) -> set[Path]:
     cache_file = directory / ".build" / target / "cache"
@@ -34,19 +62,18 @@ def collect_cache(directory: Path, target: str) -> set[Path]:
     except FileNotFoundError:
         cache_dict = dict()
 
-    cache = set()
-    recompile_all = False
-    if any(
-        cache_dict.get(file, 0) < file.stat().st_mtime
-        for file in (directory / "src").rglob("*.h")
-    ):
-        recompile_all = True
 
-    for file in (directory / "src").rglob("*.c"):
-        if recompile_all or cache_dict.get(file, 0) < file.stat().st_mtime:
-            cache.add(file)
+    files = list((directory / "src").rglob("*.[h|c]"))
+    changed_files = {file for file in files if cache_dict.get(file, 0) < file.stat().st_mtime}
+    dep_tree = build_dep_tree(files)
 
-    return cache
+    need_recompilation = set()
+    for file in filter(lambda file: file.name.endswith(".c"), files):
+        if has_dep_that_changed(file, dep_tree, changed_files) or file in changed_files:
+            print(file)
+            need_recompilation.add(file)
+
+    return need_recompilation
 
 
 @dataclass(frozen=True)
