8d0033262bdc8187a4009ae57689e2839209b64a
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-01-02 10:07:24 +0100

    fixed caching of files if runtime error

--- pybuildc/main.py
+++ pybuildc/main.py
@@ -20,9 +20,10 @@ def pybuildc(args: ArgsConfig, argv: list[str]):
             config = ConfigFile.load(args.dir, args.build_dir, args.mode)
             config.exe = args.exe
             config.bin = "exe"
-            subprocess.run([build(config, []), *argv])
-
             config.save_cache()
+            subprocess.run([build(config, []), *argv], check=True)
+
+            
         case "test":
             config = ConfigFile.load(args.dir, args.build_dir, args.mode)
             test(config)
@@ -46,4 +47,4 @@ def main():
         pybuildc(args, argv)
     except subprocess.CalledProcessError as e:
         failed_cmd = e.args[1]
-        print(f"[pybuildc] Error: '{' '.join(failed_cmd)}'")
+        print(f"[pybuildc] Error: '{' '.join(map(str, failed_cmd))}'")
