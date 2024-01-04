e1f20b22d1333c13a2f6c183329937041d70c39e
Loris Kriyonas <loris.kriyonas@gmail.com>
2024-01-04 21:31:52 +0100

    Fixed bug when renaming a .h file would not rebuild the project

--- pybuildc/cache.py
+++ pybuildc/cache.py
@@ -9,47 +9,27 @@ from pybuildc.files import Files
 DepTree = dict[Path, list[Path]]
 
 
-def _get_dep_of_file(file: Path, include_dirs: tuple[Path, ...]) -> list[Path]:
-    l = list()
-    with file.open(encoding="utf-8") as f:
-        for line in f.readlines():
-            if line.startswith("#include") and line.count('"') == 2:
-                idx = line.index('"') + 1
-                include = line[idx : line.index('"', idx)]
-                for includes in include_dirs:
-                    idir = includes / include
-                    if idir.exists():
-                        l.append(idir)
-                        l.extend(_get_dep_of_file(idir, include_dirs))
-    return l
-
-
-def dependency_tree(files: Files, include_dirs: tuple[Path, ...]) -> DepTree:
-    deps: DepTree = defaultdict(list)
-
-    for file in files.all_files:
-        deps[file] = _get_dep_of_file(file, include_dirs)
-
-    return deps
-
-
 class Cache:
-    def __init__(self, files: Files, filename: Path, deps: DepTree):
+    def __init__(self, files: Files, filename: Path, include_dirs: tuple[Path, ...]):
         self.filename = filename
-        self.deps = deps
+        self.cache: set[Path] = set()
+        self.deps = self.dependency_tree(files, include_dirs)
         try:
             self.file_m_times = pickle.loads(filename.read_bytes())
         except FileNotFoundError:
             self.file_m_times = {}
 
-        self.cache = {
+        self.cache.update({
             f
             for f in files.all_files
             if self.file_m_times.get(f, 0) < f.stat().st_mtime
             or any(
-                map(lambda i: self.file_m_times.get(i, 0) < i.stat().st_mtime, deps[f])
+                map(
+                    lambda i: self.file_m_times.get(i, 0) < i.stat().st_mtime,
+                    self.deps[f],
+                )
             )
-        }
+        })
 
     def __contains__(self, key) -> bool:
         return key in self.cache
@@ -62,7 +42,33 @@ class Cache:
                 file_m_times[f] = f.stat().st_mtime
         self.filename.write_bytes(pickle.dumps(file_m_times))
 
+    def _get_dep_of_file(
+        self, file: Path, include_dirs: tuple[Path, ...]
+    ) -> list[Path]:
+        l = list()
+        with file.open(encoding="utf-8") as f:
+            for line in f.readlines():
+                if line.startswith("#include") and line.count('"') == 2:
+                    idx = line.index('"') + 1
+                    include = line[idx : line.index('"', idx)]
+                    for includes in include_dirs:
+                        include_file = includes / include
+                        if include_file.exists():
+                            l.append(include_file)
+                            l.extend(self._get_dep_of_file(include_file, include_dirs))
+                            break
+                    else:
+                        self.cache.add(file)
+        return l
+
+    def dependency_tree(self, files: Files, include_dirs: tuple[Path, ...]) -> DepTree:
+        deps: DepTree = defaultdict(list)
+
+        for file in files.all_files:
+            deps[file] = self._get_dep_of_file(file, include_dirs)
+
+        return deps
+
 
 def cache_load(files: Files, include_dirs: tuple[Path, ...], action: Action) -> Cache:
-    deps = dependency_tree(files, include_dirs)
-    return Cache(files, files.build / f"{action}.pck", deps)
+    return Cache(files, files.build / f"{action}.pck", include_dirs)
